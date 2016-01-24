547c89369a8df96f4a9ef10dd98b4ee14f3c84c7
Ary Borenszweig <aborenszweig@manas.com.ar>
2016-01-24 10:30:35 -0300

    Fixed LibClang binding

--- src/clang/lib_clang.cr
+++ src/clang/lib_clang.cr
@@ -2,8 +2,8 @@ require "./enums"
 
 @[Link("clang")]
 ifdef darwin
-  @[Link(ldflags: "-L`xcode-select --print-path`/Toolchains/XcodeDefault.xctoolchain/usr/lib")]
-  @[Link(ldflags: "-rpath `xcode-select --print-path`/Toolchains/XcodeDefault.xctoolchain/usr/lib")]
+  @[Link(ldflags: "-L`xcode-select --print-path`/usr/lib")]
+  @[Link(ldflags: "-rpath `xcode-select --print-path`/usr/lib")]
 else
   @[Link(ldflags: "`llvm-config-3.6 --ldflags 2>/dev/null || llvm-config-3.5 --ldflags 2>/dev/null || llvm-config --ldflags 2>/dev/null`")]
 end
