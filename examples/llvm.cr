1f3d115547aec439ed5347df4bec06b36a8ea3ae
Johannes Müller <straightshoota@gmail.com>
2024-10-14 13:17:49 +0200

    `crystal tool format` with Crystal 1.15.0-dev (#82)

--- examples/llvm.cr
+++ examples/llvm.cr
@@ -7,7 +7,7 @@
   "llvm-c/Initialization.h",
   flags: "-I/usr/local/Cellar/llvm/3.6.2/include  -fPIC -fvisibility-inlines-hidden -Wall -W -Wno-unused-parameter -Wwrite-strings -Wcast-qual -Wmissing-field-initializers -pedantic -Wno-long-long -Wcovered-switch-default -Wnon-virtual-dtor -std=c++11   -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS",
   prefix: %w(LLVM_ LLVM)
-  )]
+)]
 @[Link("stdc++")]
 @[Link(ldflags: "`(llvm-config-3.6 --libs --system-libs --ldflags 2> /dev/null) || (llvm-config-3.5 --libs --system-libs --ldflags 2> /dev/null) || (llvm-config --libs --system-libs --ldflags 2>/dev/null)`")]
 lib LibLLVM
